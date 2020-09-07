67eecbe3213da78e82398aace1805917765dbae2
Youssef Ali <ymali@mun.ca>
2020-09-07 13:34:41 -0230

    added images to card tops

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,36 +6,40 @@
 
 <div class="album bg-light">
 <div class="container justify-content-center">
-<form method="GET" action="/search" class="py-2">
-<div class="input-group mb-3">
-    <input type="text" class="form-control" name="movieName" id="movieName" value = {{$movieName}}>
-        <div class="input-group-append">
-            <button class="btn btn-outline-primary" type="submit">Search OMDB</button>
+    <form method="GET" action="/search" class="py-2">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" name="movieName" id="movieName" value = {{$movieName}}>
+            <input type="hidden" id="page" name="page" value="1">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="submit">Search OMDB</button>
+                </div>
         </div>
-</div>
-</form>
+    </form>
 
-<div class="row">
-@foreach($movies as $movie)
-    <div class="col-md-6 col-lg-3 py-2">
-        <div class="card">
-            <div class="card-header text-center">
-                <p>{{$movie->Title}}</p>
-            </div>
-            <div class="card-body">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <a type="button" href="/pis}" class="btn btn-sm btn-outline-info">View</a>
-                        <a role="button" href="/pisedit" class="btn btn-sm btn-outline-info">Edit</a>
+    <div class="row">
+    @if($movies)    
+        @foreach($movies as $movie)
+            <div class="col-md-6 col-lg-3 py-2">
+                <div class="card">
+                    <img src={{$movie->Poster}} class="card-img-top img-fluid"/>
+                    <div class="card-body">
+                    <p>{{$movie->Title}} ({{$movie->Year}})</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a type="button" href="/" class="btn btn-sm btn-outline-info">View</a>
+                                <a role="button" href="/" class="btn btn-sm btn-outline-info">Edit</a>
+                            </div>
+                        <small class="text-muted"></small>
+                        </div>
                     </div>
-                <small class="text-muted"></small>
                 </div>
             </div>
-        </div>
+            
+        @endforeach
+    @else
+    not found
+    @endif
     </div>
-    
-@endforeach
-</div>
 </div>
 </div>
 </section>
