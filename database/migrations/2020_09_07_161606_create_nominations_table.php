691dd7b66513d2367f0f5b1a3485b4d45709527f
Youssef Ali <ymali@mun.ca>
2020-09-07 23:32:08 -0230

    disable the user_id foreign key

--- database/migrations/2020_09_07_161606_create_nominations_table.php
+++ database/migrations/2020_09_07_161606_create_nominations_table.php
@@ -16,20 +16,20 @@ class CreateNominationsTable extends Migration
         Schema::create('nominations', function (Blueprint $table) {
             $table->id();
 						$table->json('movies')->nullable();
-						$table->bigInteger('user_id')->unsigned()->nullable()->default(1);
+            //$table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned()->nullable()->default(1);
 						$table->timestamps();
 
-						//$table->foreign('user_id')->references('id')->on('users');
 				});
 
 				Schema::create('movies', function (Blueprint $table) {
 					$table->id();
-					$table->bigInteger('user_id')->unsigned()->nullable()->default(1);
+          //$table->foreignId('user_id')->constrained()->onDelete('cascade');
+          $table->bigInteger('user_id')->unsigned()->nullable()->default(1);
           $table->json('omdbData');
           $table->string('imdbId');
 					$table->timestamps();
 
-					//$table->foreign('user_id')->references('id')->on('users');
 			});
     }
 
