f5cba7b93d290bb0ea5a6d70070d002215f84067
Youssef Ali <ymali@mun.ca>
2020-09-07 23:41:01 -0230

    revoke the upvotes system

--- app/Nomination.php
+++ app/Nomination.php
@@ -6,25 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nomination extends Model
 {
-    public function upvote()
-    {
-        $this->votes()->updateOrCreate([
-            'user_id' => auth()->id(),
-        ],[
-            'vote' => true
-        ]);
-    }
-    
-    public function downvote()
-    {
-        $this->votes()->updateOrCreate([
-            'user_id' => auth()->id(),
-        ],[
-            'vote' => false
-        ]);
-    }
-    public function votes(){
-        return $this->hasMany(Vote::class);
-    }
-
+    //
 }
